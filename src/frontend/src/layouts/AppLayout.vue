cd207ce66b37537f1f97ec0082ca1a3af9db89f5
weezart <weezart@yandex.ru>
2022-10-05 17:15:53 +0300

    1.1 Добавлена страница констуктора пиццы пока без подключения данных.

---
+++ src/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-layout">
+    <AppLayoutHeader />
+    <IndexHome />
+  </div>
+</template>
+
+<script>
+import AppLayoutHeader from "@/layouts/AppLayoutHeader";
+import IndexHome from "@/views/Index";
+
+export default {
+  name: "AppLayout",
+  components: {
+    AppLayoutHeader,
+    IndexHome,
+  },
+};
+</script>
